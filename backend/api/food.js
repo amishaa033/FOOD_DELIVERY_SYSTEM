06cef8f3e48e419773b9d7734b4d8802d9d1afef
amishaa033 <amishak7261@gmail.com>
2025-09-17 10:14:27 +0530

    modified

--- backend/api/food.js
+++
@@ -1,5 +0,0 @@
-
-import app from "./index.js";
-import { createServer } from "@vercel/node";
-
-export default createServer(app);
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import { connectDB } from "./config/db.js";
+import FoodRouter from "./routes/FoodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config.js'; 
+import cartRouter from "./routes/cartRoute.js";
+ import orderRouter from "./routes/orderRoute.js";
+  //app config
+    const app=express(); 
+    const port=4000 
+    //middleware
+    app.use(express.json())
+     app.use(cors()) 
+     //DB connection 
+     connectDB();
+      //api endpoint 
+      app.use("/api/food",FoodRouter); 
+      app.use("/images",express.static("uploads"))
+       app.get("/",(req,res)=>{ res.send("API WORKING") })
+        app.use("/api/user",userRouter); 
+        app.use("/api/cart",cartRouter); 
+        app.use("/api/order",orderRouter); 
+        app.listen(port,()=>{ console.log(`Server started on http://localhost:${port}`); })

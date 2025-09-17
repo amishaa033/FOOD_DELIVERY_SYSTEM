86cb203bea5aa9c53abdb58df45c8445c453253b
amishaa033 <amishak7261@gmail.com>
2025-09-17 10:21:09 +0530

    modified

--- frontend/src/Context/storeContext.jsx
+++ frontend/src/Context/storeContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
     const [cartItems,setCartItems]=useState({})
-    const url="http://localhost:4000";
+    const url="https://food-delivery-system-kkt9.onrender.com";
     const [token,setToken]=useState("");
     const [food_list,setFood_list]=useState([]);
     const addToCart=async (itemId)=>{
